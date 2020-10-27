d740f307572324ebf506c218b70ea3c45ba882c5
JustinBarsalou <justinbarsalou@gmail.com>
2020-10-27 17:54:00 -0500

    generator works, styling now. cant get the joke to stay in card so going to use a template card and hope that fixes it\

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   state = {
-    yomomma: '',
+    chuck: '',
+  };
+
+  componentDidMount() {
+    console.log("Mounted up")
+    this.fetchChuck();
   }
 
-  // componentDidMount() {
-  //   
-  // }
+  fetchChuck = () => {
+    axios.get(`https://api.chucknorris.io/jokes/random`)
+    .then(response => {
+      console.log("response:", response.data.value);
+      this.setState({ chuck: response.data.value });
+    })
+    .catch ((error) => {
+      console.log(error)
+    })
+
+  }
 
 
   render() {
+    const { chuck } = this.state;
     return (
-      <div className="app">
-        hello
+      <div className="App">
+        <div className="joke-card">
+
+          <h1 className="heading"> Joke: </h1>
+          <h1 className="joke-itself">{chuck}</h1>
+
+
+        </div>
       </div>
     );
   }
